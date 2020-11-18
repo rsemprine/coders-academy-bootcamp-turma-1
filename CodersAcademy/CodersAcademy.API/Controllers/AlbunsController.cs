45fd0eaadf9204b285824cf183a5da4c05e7a30d
Rodrigo Semprine dos Santos Fonseca <odrigo.fonseca@d0007azurecloudtotvscom.onmicrosoft.com>
2020-11-18 20:32:03 -0300

    added migrations and mappings

---
+++ CodersAcademy/CodersAcademy.API/Controllers/AlbunsController.cs
@@ -0,0 +1,28 @@
+﻿using CodersAcademy.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbunsController : ControllerBase
+    {
+        private AlbumRepository Repository { get; init; }
+
+        public AlbunsController(AlbumRepository repository)
+        {
+            Repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAlbuns()
+        {
+            return Ok(await Repository.GetAllAsync());
+        }
+    }
+}
---
+++ CodersAcademy/CodersAcademy.API/Repository/AlbumRepository.cs
@@ -0,0 +1,23 @@
+﻿using CodersAcademy.API.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.API.Repository
+{
+    public class AlbumRepository
+    {
+        private MusicContext Context { get; init; }        
+
+        public AlbumRepository(MusicContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<IList<Album>> GetAllAsync()
+            => await Context.Albuns.ToListAsync();
+        
+    }
+}
